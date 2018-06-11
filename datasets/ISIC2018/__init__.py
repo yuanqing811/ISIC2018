5861d69e443910e1cc3437704ce6132c75454fff
Saurabh Tavildar <tavildar@gmail.com>
2018-06-10 20:05:53 -0700

    Added support to resize and save task 12 data

--- datasets/ISIC2018/__init__.py
+++ datasets/ISIC2018/__init__.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 from skimage import io
 from skimage import transform
 from paths import root_dir, mkdir_if_not_exist
+import pandas as pd
+from PIL import Image
 
 ISIC2018_dir = os.path.join(root_dir, 'datasets', 'ISIC2018')
 data_dir = os.path.join(ISIC2018_dir, 'data')
@@ -17,13 +19,13 @@ task1_gt = 'ISIC2018_Task1_Training_GroundTruth'
 task2_gt = 'ISIC2018_Task2_Training_GroundTruth_v3'
 task3_gt = 'ISIC2018_Task3_Training_GroundTruth'
 
-MEL = 0     # Melanoma
-NV = 1      # Melanocytic nevus
-BCC = 2     # Basal cell carcinoma
-AKIEC = 3   # Actinic keratosis / Bowen's disease (intraepithelial carcinoma)
-BKL = 4     # Benign keratosis (solar lentigo / seborrheic keratosis / lichen planus-like keratosis)
-DF = 5      # Dermatofibroma
-VASC = 6    # Vascular lesion
+MEL = 0  # Melanoma
+NV = 1  # Melanocytic nevus
+BCC = 2  # Basal cell carcinoma
+AKIEC = 3  # Actinic keratosis / Bowen's disease (intraepithelial carcinoma)
+BKL = 4  # Benign keratosis (solar lentigo / seborrheic keratosis / lichen planus-like keratosis)
+DF = 5  # Dermatofibroma
+VASC = 6  # Vascular lesion
 
 classes = [MEL, NV, BCC, AKIEC, BKL, DF, VASC]
 class_names = ['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC']
@@ -48,6 +50,7 @@ if os.path.isdir(task3_img_dir):
     task3_image_ids.sort()
 
 task3_gt_fname = 'ISIC2018_Task3_Training_GroundTruth.csv'
+task3_sup_fname = 'ISIC2018_Task3_Training_LesionGroupings.csv'
 
 task12_images_npy_prefix = 'task12_images'
 task3_images_npy_prefix = 'task3_images'
@@ -76,7 +79,7 @@ ATTRIBUTES = {
 }
 
 
-def load_image_by_id(image_id, fname_fn, from_dir, output_size=None):
+def load_image_by_id(image_id, fname_fn, from_dir, output_size=None, save_resized=False, to_dir=None):
     img_fnames = fname_fn(image_id)
     if isinstance(img_fnames, str):
         img_fnames = [img_fnames, ]
@@ -85,15 +88,25 @@ def load_image_by_id(image_id, fname_fn, from_dir, output_size=None):
 
     images = []
     for img_fname in img_fnames:
-        img_fname = os.path.join(from_dir, img_fname)
-        image = io.imread(img_fname)
+        img_path_in = os.path.join(from_dir, img_fname)
+        if not os.path.exists(img_path_in):
+            raise FileNotFoundError('img %s not found' % img_fname)
+        image = io.imread(img_path_in)
+
         if output_size:
             image = transform.resize(image, (output_size, output_size),
                                      order=1, mode='constant',
                                      cval=0, clip=True,
                                      preserve_range=True,
                                      anti_aliasing=True)
+
         image = image.astype(np.uint8)
+
+        if output_size and save_resized:
+            img_path_out = os.path.join(to_dir, img_fname)
+            im = Image.fromarray(image)
+            im.save(img_path_out)
+
         images.append(image)
 
     if len(images) == 1:
@@ -206,39 +219,33 @@ def load_task3_training_labels():
     return labels
 
 
-def load_training_data(task_idx,
-                       output_size=None,
-                       num_partitions=5,
-                       idx_partition=0,
-                       test_split=0.):
-    assert isinstance(task_idx, int) and 0 < task_idx <= 3
-    if task_idx == 1:
-        x = load_task12_training_images(output_size=output_size)
-        y = load_task1_training_masks(output_size=output_size)
-    elif task_idx == 2:
-        x = load_task12_training_images(output_size=output_size)
-        y = load_task2_training_masks(output_size=output_size)
-    else:
-        x = load_task3_training_images(output_size=output_size)
-        y = load_task3_training_labels()
-
-    return partition_data(x=x, y=y, k=num_partitions, i=idx_partition, test_split=test_split)
-
-
-def partition_data(x, y, k=5, i=0, test_split=1./6, seed=42):
-
+def partition_task3_data(x, y, k=5, i=0, test_split=1. / 6, seed=42):
     assert isinstance(k, int) and isinstance(i, int) and 0 <= i < k
 
-    n = x.shape[0]
+    fname = os.path.join(task3_gt_dir, task3_sup_fname)
+    assert os.path.exists(fname)
 
+    df = pd.read_csv(os.path.join(task3_gt_dir, task3_sup_fname))
+    grouped = df.groupby('lesion_id', sort=True)
+    lesion_ids = []
+    for name, group in grouped:
+        image_ids = group.image.tolist()
+        lesion_ids.append([name, image_ids])
+
+    # shuffle lesion ids
+    np.random.seed(seed)
+    n = len(lesion_ids)
+    indices = np.random.permutation(n)
+
+    image_ids = [image_id for idx in indices for image_id in lesion_ids[idx][1]]
+    n = len(image_ids)
     n_set = int(n * (1. - test_split)) // k
     # divide the data into (k + 1) sets, -1 is test set, [0, k) are for train and validation
-    indices = np.array([i for i in range(k) for _ in range(n_set)] +
-                       [-1] * (n - n_set * k),
-                       dtype=np.int8)
+    indices = [i for i in range(k) for _ in range(n_set)] + [-1] * (n - n_set * k)
 
-    np.random.seed(seed)
-    np.random.shuffle(indices)
+    indices = list(zip(indices, image_ids))
+    indices.sort(key=lambda x: x[1])
+    indices = np.array([idx for idx, image_id in indices], dtype=np.uint)
 
     valid_indices = (indices == i)
     test_indices = (indices == -1)
@@ -254,3 +261,115 @@ def partition_data(x, y, k=5, i=0, test_split=1./6, seed=42):
     y_test = y[test_indices]
 
     return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
+
+
+def load_training_data(task_idx,
+                       output_size=None,
+                       num_partitions=5,
+                       idx_partition=0,
+                       test_split=0.):
+    assert isinstance(task_idx, int) and 0 < task_idx <= 3
+    if task_idx == 1:
+        x = load_task12_training_images(output_size=output_size)
+        y = load_task1_training_masks(output_size=output_size)
+        return partition_data(x=x, y=y, k=num_partitions, i=idx_partition, test_split=test_split)
+
+    elif task_idx == 2:
+        x = load_task12_training_images(output_size=output_size)
+        y = load_task2_training_masks(output_size=output_size)
+        return partition_data(x=x, y=y, k=num_partitions, i=idx_partition, test_split=test_split)
+
+    else:
+        x = load_task3_training_images(output_size=output_size)
+        y = load_task3_training_labels()
+        return partition_task3_data(x=x, y=y, k=num_partitions, i=idx_partition, test_split=test_split)
+
+
+def partition_indices(n, k=5, i=0, test_split=1. / 6, seed=42):
+    n_set = int(n * (1. - test_split)) // k
+    # divide the data into (k + 1) sets, -1 is test set, [0, k) are for train and validation
+    indices = np.array([i for i in range(k) for _ in range(n_set)] +
+                       [-1] * (n - n_set * k),
+                       dtype=np.int8)
+
+    np.random.seed(seed)
+    np.random.shuffle(indices)
+
+    valid_indices = (indices == i)
+    test_indices = (indices == -1)
+    train_indices = ~(valid_indices | test_indices)
+
+    return train_indices, valid_indices, test_indices
+
+
+def partition_data(x, y, k=5, i=0, test_split=1. / 6, seed=42):
+    assert isinstance(k, int) and isinstance(i, int) and 0 <= i < k
+
+    train_indices, valid_indices, test_indices = partition_indices(n=x.shape[0], k=k, i=i,
+                                                                   test_split=test_split, seed=seed)
+
+    x_valid = x[valid_indices]
+    y_valid = y[valid_indices]
+
+    x_train = x[train_indices]
+    y_train = y[train_indices]
+
+    x_test = x[test_indices]
+    y_test = y[test_indices]
+
+    return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
+
+
+def get_task12_resize_img_dir(output_size=None):
+
+    task12_img_dir_resized = task12_img_dir.replace('datasets/ISIC2018/data', 'datasets/ISIC2018/cache') \
+                             + '_resize_' + str(output_size)
+
+    task1_gt_dir_resized = task1_gt_dir.replace('datasets/ISIC2018/data', 'datasets/ISIC2018/cache') \
+                           + '_resize_' + str(output_size)
+
+    task2_gt_dir_resized = task2_gt_dir.replace('datasets/ISIC2018/data', 'datasets/ISIC2018/cache') \
+                           + '_resize_' + str(output_size)
+
+    return task12_img_dir_resized, task1_gt_dir_resized, task2_gt_dir_resized
+
+
+def resize_and_save_task12(output_size=None):
+
+    task12_img_dir_resized, task1_gt_dir_resized, task2_gt_dir_resized = get_task12_resize_img_dir(
+        output_size=output_size)
+
+    mkdir_if_not_exist([task12_img_dir_resized, task1_gt_dir_resized, task2_gt_dir_resized])
+
+    image_id_list = task12_image_ids.copy()
+
+    for image_id in tqdm(image_id_list):
+
+        _ = load_image_by_id(image_id,
+                             from_dir=task12_img_dir,
+                             output_size=output_size,
+                             fname_fn=lambda x: '%s.jpg' % x,
+                             save_resized=True,
+                             to_dir=task12_img_dir_resized)
+
+        _ = load_image_by_id(image_id,
+                             from_dir=task1_gt_dir,
+                             output_size=output_size,
+                             fname_fn=lambda x: '%s_segmentation.png' % x,
+                             save_resized=True,
+                             to_dir=task1_gt_dir_resized)
+
+        _ = load_image_by_id(image_id,
+                             from_dir=task2_gt_dir,
+                             output_size=output_size,
+                             fname_fn=lambda x: ('%s_attribute_globules.png' % x,
+                                                 '%s_attribute_milia_like_cyst.png' % x,
+                                                 '%s_attribute_negative_network.png' % x,
+                                                 '%s_attribute_pigment_network.png' % x,
+                                                 '%s_attribute_streaks.png' % x),
+                             save_resized=True,
+                             to_dir=task2_gt_dir_resized)
+
+
+if __name__ == "__main__":
+    resize_and_save_task12(output_size=1024)
--- datasets/ISIC2018/preprocess_data.py
+++ datasets/ISIC2018/preprocess_data.py
@@ -1,6 +1,9 @@
 if __name__ == '__main__':
 
-    from datasets.ISIC2018 import load_training_data
+    from datasets.ISIC2018 import load_training_data, resize_and_save_task12
+
+    resize_and_save_task12(output_size=1024)
+
     _, _, _ = load_training_data(task_idx=1, output_size=224)
     _, _, _ = load_training_data(task_idx=2, output_size=224)
     _, _, _ = load_training_data(task_idx=3, output_size=224)
