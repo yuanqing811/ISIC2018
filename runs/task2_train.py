a7287636be6a611f15ad02c48242875415f8235a
Qing Yuan <yuan.qing.bio@gmail.com>
2018-06-16 22:49:00 -0700

    Merge branch 'focal-loss-experiment' of https://github.com/yuanqing811/ISIC2018 into focal-loss-experiment
    # Conflicts:
    #	runs/task1_eval.py
    #	runs/task2_train.py

--- runs/task2_train.py
+++ runs/task2_train.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     from keras.optimizers import Adam
     from keras.initializers import RandomNormal
     from models import backbone
+    from models.submodels.segmentation import decoder2
     import sys
 
     input_shape = (1024, 1024, 3)
@@ -90,6 +91,7 @@ if __name__ == '__main__':
     else:
         model = backbone(backbone_name, **backbone_options).segmentation_model(input_shape=input_shape,
                                                                                num_classes=1,
+                                                                               submodel=decoder2,
                                                                                upsampling_type=upsampling_type,
                                                                                bottleneck=bottleneck,
                                                                                blocks=blocks,
